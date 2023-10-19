c6387170f8419ac29da956cba52e62db49cedaf2
MARCELO OJEDA <marcelo.ojeda@uc.cl>
2023-10-18 23:47:16 -0300

    tarea2estelar

---
+++ AST0321 - Astrofísica Estelar/TAREA 2/MESA/multicolor_label.py
@@ -0,0 +1,29 @@
+from matplotlib.offsetbox import AnchoredOffsetbox, TextArea, HPacker, VPacker
+
+def multicolor_ylabel(ax,list_of_strings,list_of_colors,axis='x',anchorpad=0,**kw):
+    """this function creates axes labels with multiple colors
+    ax specifies the axes object where the labels should be drawn
+    list_of_strings is a list of all of the text items
+    list_if_colors is a corresponding list of colors for the strings
+    axis='x', 'y', or 'both' and specifies which label(s) should be drawn"""
+
+    # x-axis label
+    if axis=='x' or axis=='both':
+        boxes = [TextArea(text, textprops=dict(color=color, ha='left',va='bottom',**kw)) 
+                    for text,color in zip(list_of_strings,list_of_colors) ]
+        xbox = HPacker(children=boxes,align="center",pad=0, sep=5)
+        anchored_xbox = AnchoredOffsetbox(loc=3, child=xbox, pad=anchorpad,frameon=False,bbox_to_anchor=(0.2, -0.09),
+                                          bbox_transform=ax.transAxes, borderpad=0.)
+        ax.add_artist(anchored_xbox)
+
+    # y-axis label
+    if axis=='y' or axis=='both':
+        boxes = [TextArea(text, textprops=dict(color=color, ha='left',va='bottom',rotation=90,**kw)) 
+                     for text,color in zip(list_of_strings[::-1],list_of_colors) ]
+        ybox = VPacker(children=boxes,align="center", pad=0, sep=3)
+        anchored_ybox = AnchoredOffsetbox(loc='upper left', child=ybox, pad=anchorpad, frameon=False, bbox_to_anchor=(0.10, 1), 
+                                          bbox_transform=ax.transAxes, borderpad=0.)
+        ax.add_artist(anchored_ybox)
+        
+def test():
+    return
